8e60cbdbb0d4488adf48633c406de327ed2ec30b
youngandmad <kkraken2005@gmail.com>
2023-12-31 02:50:48 +0600

    separated RabbitProperties

---
+++ mail-service/src/main/java/socialapp/mailservice/config/rabbit/ConnectionConfig.java
@@ -0,0 +1,75 @@
+package socialapp.mailservice.config.rabbit;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import socialapp.mailservice.consumer.advice.RabbitErrorHandler;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(RabbitProperties.class)
+public class ConnectionConfig {
+
+    private final RabbitProperties rabbitProperties;
+
+    /**
+     * Configures a Jackson2JsonMessageConverter as the default message converter for RabbitMQ.
+     *
+     * @return Jackson2JsonMessageConverter instance
+     */
+    @Bean
+    MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+
+    /**
+     * Configures a RabbitMQ connection factory with the specified host, username, and password.
+     *
+     * @return ConnectionFactory instance
+     */
+    @Bean
+    ConnectionFactory connectionFactory() {
+        var fct = new CachingConnectionFactory(rabbitProperties.getHost());
+
+        fct.setUsername(rabbitProperties.getUsername());
+        fct.setPassword(rabbitProperties.getPassword());
+
+        return fct;
+    }
+
+    /**
+     * Configures a {@code SimpleRabbitListenerContainerFactory} bean for handling RabbitMQ message listeners.
+     * This factory is responsible for creating {@code SimpleRabbitListenerContainer} instances used to
+     * asynchronously consume messages from RabbitMQ queues.
+     *
+     * @param connectionFactory       The {@code ConnectionFactory} used for creating connections to RabbitMQ.
+     * @param configurer              The {@code SimpleRabbitListenerContainerFactoryConfigurer} for configuring the factory.
+     * @param errorHandler            The custom {@code RabbitErrorHandler} for handling errors during message consumption.
+     * @return A configured {@code SimpleRabbitListenerContainerFactory} bean.
+     * @see SimpleRabbitListenerContainerFactory
+     * @see SimpleRabbitListenerContainerFactoryConfigurer
+     * @see RabbitErrorHandler
+     */
+    @Bean
+    SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            SimpleRabbitListenerContainerFactoryConfigurer configurer,
+            RabbitErrorHandler errorHandler
+    ) {
+        var factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setErrorHandler(errorHandler);
+        return factory;
+    }
+
+
+}
---
+++ mail-service/src/main/java/socialapp/mailservice/config/rabbit/RabbitProperties.java
@@ -0,0 +1,38 @@
+package socialapp.mailservice.config.rabbit;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.rabbitmq")
+@Data
+@Slf4j
+public class RabbitProperties {
+
+    private Headers headers;
+    private Long baseTtl;
+    private Queues queues;
+    private String username;
+    private String password;
+    private String host;
+
+    @PostConstruct
+    void init() {
+        log.debug("RabbitTopics initialized {}", this);
+    }
+
+    @Data
+    public static class Headers {
+        private String xDeadLetterExchange;
+        private String xDeadLetterRoutingKey;
+    }
+
+    @Data
+    public static class Queues {
+        private String newsLetterName;
+        private String newsLetterExchange;
+        private String newsLetterRoutingKey;
+    }
+
+}
