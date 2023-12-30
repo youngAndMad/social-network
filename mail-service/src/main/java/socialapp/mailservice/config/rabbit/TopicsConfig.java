5b3f074d89fb21cc766757ef9d7623ca9a80ac36
Daneker Kaliaskaruly <113981956+youngAndMad@users.noreply.github.com>
2023-12-31 04:34:47 +0600

    Ftr mail service (#3)
    * mail service init
    
    * base ftl && rabbit configs
    
    * newsletter typed messages sending
    
    * error handler and docker-compose yml with service rabbit
    
    * separated RabbitProperties
    
    * delete intential dlq bean
    
    * fixed java mail sender bean
    
    * fixed queue name

---
+++ mail-service/src/main/java/socialapp/mailservice/config/rabbit/TopicsConfig.java
@@ -0,0 +1,70 @@
+package socialapp.mailservice.config.rabbit;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Configuration class for RabbitMQ integration.
+ * Enables RabbitMQ functionality and configures message converters.
+ *
+ * @author Daneker
+ */
+@Configuration
+@EnableRabbit
+@RequiredArgsConstructor
+public class TopicsConfig {
+
+
+    private final RabbitProperties rabbitProperties;
+
+    /**
+     * Represents a Spring bean for creating a queue named "newsLetterQueue" with durability enabled.
+     *
+     * @return A new instance of the "newsLetterQueue" queue.
+     */
+    @Bean
+    Queue newsLetterQueue() {
+        return new Queue(rabbitProperties.getQueues().getNewsLetterName(), true);
+    }
+
+
+    /**
+     * Represents a Spring bean for creating a direct exchange named "newsLetterExchange" with durability enabled.
+     *
+     * @return A new instance of the "newsLetterExchange" direct exchange.
+     */
+    @Bean
+    Exchange newsLetterExchange() {
+        return ExchangeBuilder
+                .directExchange(rabbitProperties.getQueues().getNewsLetterExchange())
+                .durable(true)
+                .build();
+    }
+
+
+    /**
+     * Represents a Spring bean for creating a binding between the "newsLetterQueue" and "newsLetterExchange"
+     * with the specified routing key ("newsLetterRoutingKey").
+     *
+     * @param queue    The "newsLetterQueue" queue instance.
+     * @param exchange The "newsLetterExchange" exchange instance.
+     * @return A new binding between the queue and exchange with the specified routing key and no additional arguments.
+     */
+    @Bean
+    Binding newsLetterBinding(
+            @Qualifier("newsLetterQueue") Queue queue,
+            @Qualifier("newsLetterExchange") Exchange exchange
+    ) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(rabbitProperties.getQueues().getNewsLetterRoutingKey())
+                .noargs();
+    }
+
+}
---
+++ mail-service/src/main/resources/templates/news_letter.ftl
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NewsLetter</title>
+    <style>
+        #subject {
+            color: #000000;
+            font-size: 20px;
+            font-weight: bold;
+            text-align: center;
+        }
+        .content {
+            color: #000000;
+            font-size: 16px;
+            text-align: justify;
+        }
+    </style>
+</head>
+<body>
+    <h1 id="subject">${subject}</h1>
+    <p class="content">${content}</p>
+</body>
+</html>
