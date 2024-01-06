a7922d0283496d484d9cc034aa05315de17e2045
youngandmad <kkraken2005@gmail.com>
2024-01-06 17:50:19 +0600

    liquibase users table migration

--- auth-service/src/main/java/socialapp/authservice/model/entity/User.java
+++ auth-service/src/main/java/socialapp/authservice/model/entity/User.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 @Table(name = "users")
 @Setter
-public class User {
+public class User extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
---
+++ auth-service/src/main/resources/db.changelog/changeset/db.changelog-1-create-users-table.sql
@@ -0,0 +1,16 @@
+--liquibase formatted sql
+--changeset daneker:create-users-table
+CREATE TABLE users
+(
+    id         bigserial PRIMARY KEY,
+    first_name varchar(255)        NOT NULL,
+    last_name  varchar(255)        NOT NULL,
+    username   varchar(255) UNIQUE NOT NULL,
+    password   varchar(255)        NOT NULL,
+    tag        varchar(255) UNIQUE NOT NULL,
+    email      varchar(255) UNIQUE NOT NULL,
+    phone      varchar(255),
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone
+);
+--rollback drop table users;
