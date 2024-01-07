21e3bc5eae085c4c98ef0bdc86381477ed8fd093
youngandmad <kkraken2005@gmail.com>
2024-01-07 21:33:40 +0600

    migrated registered client settings into application.yml

--- auth-service/src/main/java/socialapp/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/socialapp/authservice/AuthServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import socialapp.authservice.config.OAuth2ClientProperties;
 import socialapp.authservice.config.ServerProperties;
 import socialapp.authservice.config.rabbit.RabbitProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({RabbitProperties.class, ServerProperties.class})
+@EnableConfigurationProperties(
+        {RabbitProperties.class, OAuth2ClientProperties.class, ServerProperties.class})
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- auth-service/src/main/java/socialapp/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/socialapp/authservice/config/SecurityConfig.java
@@ -1,36 +1,45 @@
 package socialapp.authservice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final OAuth2ClientProperties oauth2ClientProperties;
+
     private final static String [] WHITE_LIST = {
-            "/api/v1/auth/register","/api/v1/auth/confirm-email",  "/v3/api-docs/**", "/swagger-ui/**"
+            "/api/v1/auth/register", "/api/v1/auth/confirm-email",  "/v3/api-docs/**", "/swagger-ui/**"
     };
 
     @Bean
     RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
-        var registeredClient = RegisteredClient.withId("demo")
-                .clientId("demo")
-                .clientSecret(passwordEncoder.encode("secret"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        var registeredClient = RegisteredClient
+                .withId(oauth2ClientProperties.getId())
+                .clientId(oauth2ClientProperties.getId())
+                .clientSecret(passwordEncoder.encode(oauth2ClientProperties.getSecret()))
+                .clientAuthenticationMethod(oauth2ClientProperties.getAuthenticationMethod())
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri("http://127.0.0.1:8082/login/oauth2/code/reg-client")
-                .scope("user.read")
-                .scope("user.write")
-                .scope("openid")
+                .redirectUri(oauth2ClientProperties.getRedirectUri())
+                .scopes(scopes -> scopes.addAll(oauth2ClientProperties.getScopes()))
+                .tokenSettings(
+                        TokenSettings.builder()
+                        .accessTokenTimeToLive(oauth2ClientProperties.getTokenSettings().getAccessTokenTimeToLive())
+                        .refreshTokenTimeToLive(oauth2ClientProperties.getTokenSettings().getRefreshTokenTimeToLive())
+                        .build()
+                )
                 .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
     }
