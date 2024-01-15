9f0982d26bb5187d9f2c93d295196abd68ea81a7
Qaisar Baltabayev <127594814+qaisar04@users.noreply.github.com>
2024-01-15 12:10:57 +0600

    init logging starter (#12)

---
+++ logging-spring-boot-starter/src/main/java/socialapp/aop/config/LoggingAutoConfiguration.java
@@ -0,0 +1,43 @@
+package socialapp.aop.config;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import socialapp.aop.aspects.LoggingMethodExecutionAspect;
+import socialapp.aop.aspects.LoggingTimeExecutionAspect;
+
+@Configuration
+@EnableConfigurationProperties(LoggingProperties.class)
+@ConditionalOnClass(LoggingProperties.class)
+@ConditionalOnProperty(prefix = "app.common.logging", name = "enabled", havingValue = "true")
+@EnableAspectJAutoProxy
+public class LoggingAutoConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAutoConfiguration.class);
+
+    @PostConstruct
+    void init() {
+        logger.info("LoggingAutoConfiguration initialized");
+    }
+
+    @Bean
+    @ConditionalOnClass(LoggingMethodExecutionAspect.class)
+    public LoggingMethodExecutionAspect loggingMethodExecutionAspect() {
+        logger.info("LoggingMethodExecutionAspect bean start to create.");
+        return new LoggingMethodExecutionAspect();
+    }
+
+    @Bean
+    @ConditionalOnClass(LoggingTimeExecutionAspect.class)
+    public LoggingTimeExecutionAspect loggingTimeExecutionAspect() {
+        logger.info("LoggingTimeExecutionAspect bean start to create.");
+        return new LoggingTimeExecutionAspect();
+    }
+}
+
---
+++ logging-spring-boot-starter/src/main/java/socialapp/aop/config/LoggingProperties.java
@@ -0,0 +1,27 @@
+package socialapp.aop.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+@Data
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "app.common.logging")
+public class LoggingProperties {
+    /**
+     * to enable common logging aop on service layer
+     */
+    private boolean enabled;
+    private String level;
+
+    public static final Logger logger = LoggerFactory.getLogger(LoggingProperties.class);
+
+    @PostConstruct
+    void init() {
+        logger.info("Logging properties initialized: {}", this);
+    }
+}
