2e9826bda2d0fc47630a36060163cdb50b095375
Daneker Kaliaskaruly <kaliaskaruly@ezdrav.kz>
2024-01-15 17:36:09 +0600

    retry and circuit breaker for storage client

--- user-service/src/main/java/socialapp/userservice/common/client/StorageClient.java
+++ user-service/src/main/java/socialapp/userservice/common/client/StorageClient.java
@@ -1,5 +1,10 @@
 package socialapp.userservice.common.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +17,24 @@ import socialapp.userservice.model.dto.FileUploadResponseDto;
 @FeignClient(name = "storage-client")
 public interface StorageClient {
 
+    Logger log = LoggerFactory.getLogger(StorageClient.class);
+
     @PostMapping("single")
+    @CircuitBreaker(name = "storageService", fallbackMethod = "circuitBreakerCallback")
+    @Retry(name ="storageService")
     ResponseEntity<FileUploadResponseDto> upload(@RequestParam(value = "source", required = false, defaultValue = "USER_PROFILE_IMAGE") String source,
                                                  @RequestParam Long target,
                                                  @RequestParam("file") MultipartFile file
     );
+
+    default  ResponseEntity<FileUploadResponseDto> circuitBreakerCallback(Throwable throwable){
+        log.error("circuitBreakerCallback err = {}", throwable.getMessage());
+        return ResponseEntity.ok(null);
+    }
+
+
+    default  ResponseEntity<FileUploadResponseDto> retryCallback(Throwable throwable){
+        log.error("retryCallback err = {}", throwable.getMessage());
+        return ResponseEntity.ok(null);
+    }
 }
