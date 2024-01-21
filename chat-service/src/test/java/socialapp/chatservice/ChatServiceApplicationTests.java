238e481c3baa369e53dc39c4f7a2eaab4ffde7be
Daneker Kaliaskaruly <kaliaskaruly@ezdrav.kz>
2024-01-21 22:23:35 +0600

    chat service init

---
+++ chat-service/src/test/java/socialapp/chatservice/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package socialapp.chatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
