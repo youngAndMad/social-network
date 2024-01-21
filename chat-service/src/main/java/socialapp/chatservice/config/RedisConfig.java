46aa47b065ad4f69beec25253537b9c2e77af258
Daneker Kaliaskaruly <kaliaskaruly@ezdrav.kz>
2024-01-21 23:44:18 +0600

    redis multiple instances, chat service ci with new template

---
+++ chat-service/src/main/java/socialapp/chatservice/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package socialapp.chatservice.config;
+
+import io.lettuce.core.ReadFrom;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.util.List;
+
+@Configuration
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisConfig {
+
+    private RedisInstance master;
+    private List<RedisInstance> slaves;
+
+    @Bean
+    RedisConnectionFactory redisConnectionFactory() {
+        var clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .build();
+        var staticMasterReplicaConfiguration = new RedisStaticMasterReplicaConfiguration(master.getHost(), master.getPort());
+        slaves.forEach(slave -> staticMasterReplicaConfiguration.addNode(slave.getHost(), slave.getPort()));
+        return new LettuceConnectionFactory(staticMasterReplicaConfiguration, clientConfig);
+    }
+    @Setter
+    @Getter
+    private static class RedisInstance {
+        private String host;
+        private int port;
+    }
+}
