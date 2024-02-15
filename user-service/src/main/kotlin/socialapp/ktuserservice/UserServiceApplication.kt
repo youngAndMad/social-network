1f45d6090b162206e27149d62cb70b5ab8859d74
youngandmad <kkraken2005@gmail.com>
2024-02-15 19:05:26 +0600

    disable ssl validation user service, spring security user service

--- user-service/src/main/kotlin/socialapp/ktuserservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/socialapp/ktuserservice/UserServiceApplication.kt
@@ -3,10 +3,33 @@ package socialapp.ktuserservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 @SpringBootApplication
 @EnableFeignClients
-class UserServiceApplication
+class UserServiceApplication {
+
+    object TrustAllCertificates : X509TrustManager {
+        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) = Unit
+        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) = Unit
+        override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+    }
+
+    val allHostsValid = HostnameVerifier { _, _ -> true }
+
+    init {
+        try {
+            val sc: SSLContext = SSLContext.getInstance("SSL")
+            sc.init(null, arrayOf<TrustManager>(TrustAllCertificates), SecureRandom())
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<UserServiceApplication>(*args)
