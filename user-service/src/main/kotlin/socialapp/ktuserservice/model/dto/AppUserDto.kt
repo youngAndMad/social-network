9bda226eb109c72d37904e2778919c7307bd433a
youngandmad <kkraken2005@gmail.com>
2024-03-03 03:22:24 +0600

    fixed spring securiry swagger issue, user response dto instead of user in me endpoint

--- user-service/src/main/kotlin/socialapp/ktuserservice/model/dto/AppUserDto.kt
+++
@@ -1,9 +0,0 @@
-package socialapp.ktuserservice.model.dto
-
-
-data class AppUserDto(
-    val givenName: String,
-    val email: String,
-    val familyName: String,
-    val preferredUsername: String
-)
