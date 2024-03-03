bbbe9a775f5300dfa39572429ae1ffbc5f2f4bc9
youngandmad <kkraken2005@gmail.com>
2024-03-03 12:01:34 +0600

    comment neo4j temp

--- user-service/src/main/kotlin/socialapp/ktuserservice/config/Neo4JConfig.kt
+++ user-service/src/main/kotlin/socialapp/ktuserservice/config/Neo4JConfig.kt
@@ -1,15 +1,15 @@
-package socialapp.ktuserservice.config
-
-import org.neo4j.cypherdsl.core.renderer.Dialect
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class Neo4JConfig {
-
-    @Bean
-    fun cypherDslConfiguration(): org.neo4j.cypherdsl.core.renderer.Configuration =
-        org.neo4j.cypherdsl.core.renderer.Configuration
-            .newConfig()
-            .withDialect(Dialect.NEO4J_5).build();
-}
+//package socialapp.ktuserservice.config
+//
+//import org.neo4j.cypherdsl.core.renderer.Dialect
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//
+//@Configuration
+//class Neo4JConfig {
+//
+//    @Bean
+//    fun cypherDslConfiguration(): org.neo4j.cypherdsl.core.renderer.Configuration =
+//        org.neo4j.cypherdsl.core.renderer.Configuration
+//            .newConfig()
+//            .withDialect(Dialect.NEO4J_5).build();
+//}
